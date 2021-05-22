44239ce6cb32776d145e6b64a0736deedc9e4e89
olivia moon <moontjdgml@gmail.com>
2021-05-22 18:00:47 -0400

    inquier prompt compelte

--- index.js
+++ index.js
@@ -8,80 +8,149 @@ const inquirer = require("inquirer");
 const teamArray = [];
 
 const ManagerQuestion = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the Manager name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the Manager id?",
-    },
-    {
-        type: "email",
-        name: "name",
-        message: "What is the Manager email?",
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the Manager officeNumber?",
-    }
-]
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Manager name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the Manager id?",
+  },
+  {
+    type: "email",
+    name: "name",
+    message: "What is the Manager email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the Manager officeNumber?",
+  },
+];
 
 const InternQuestion = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the Intern name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the Intern id?",
-    },
-    {
-        type: "email",
-        name: "name",
-        message: "What is the Intern email?",
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What is the Intern school?",
-    }
-]
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Intern name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the Intern id?",
+  },
+  {
+    type: "email",
+    name: "name",
+    message: "What is the Intern email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the Intern school?",
+  },
+];
 
 const EngineerQuestion = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the Engineer name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the Engineer id?",
-    },
-    {
-        type: "email",
-        name: "name",
-        message: "What is the Engineer email?",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is the Engineer github?",
-    }
-]
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Engineer name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the Engineer id?",
+  },
+  {
+    type: "email",
+    name: "name",
+    message: "What is the Engineer email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the Engineer github?",
+  },
+];
+
+const addMoreEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "who would you like to add?",
+        name: "choice",
+        choices: ["Engineer", "Intern", "Manager", "Please no More"],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.choice) {
+        case "Engineer":
+          buildEngineerObject();
+          break;
+        case "Intern":
+          buildInternObject();
+          break;
+        case "Manager":
+          buildManagerObject();
+          break;
+        default:
+          teamBuild();
+          break;
+      }
+    });
+};
 
 const buildEngineerObject = () => {
-    inquirer.prompt(EngineerQuestion).then((answer) => {
-        //create new object
-        const newM
-        //add new object to team array
+  inquirer.prompt(EngineerQuestion).then((answer) => {
+    //create new object
+    const newEngineer = new Engineer(
+      answer.name,
+      answer.id,
+      answer.email,
+      answer.github
+    );
+    //add new object to team array
+    teamArray.push(newEngineer);
+    //add more members
+    addMoreEmployee();
+  });
+};
 
-        //add more members
-    });
-}
+const buildInternObject = () => {
+  inquirer.prompt(InternQuestion).then((answer) => {
+    //create new object
+    const newIntern = new Intern(
+      answer.name,
+      answer.id,
+      answer.email,
+      answer.school
+    );
+    //add new object to team array
+    teamArray.push(newIntern);
+    //add more members
+    addMoreEmployee();
+  });
+};
+
+const buildManagerObject = () => {
+  inquirer.prompt(ManagerQuestion).then((answer) => {
+    //create new object
+    const newManager = new Manager(
+      answer.name,
+      answer.id,
+      answer.email,
+      answer.github
+    );
+    //add new object to team array
+    teamArray.push(newManager);
+    //add more members
+    addMoreEmployee();
+  });
+};
+
+addMoreEmployee();
+
